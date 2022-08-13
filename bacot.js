7c6281b6a1bf2910775fefebd053e027521f9569
Vernando Ichsan <vernandoichsan@Vernandos-MacBook-Air.local>
2022-08-13 14:32:58 +0700

    belajarweek1

---
+++ bacot.js
@@ -0,0 +1,79 @@
+let kata1= 'nama gue bro nando, umur gue'
+let angka1=5
+let kata2='tahun lagi jadi'
+let angka2=3
+let kata3='puluh'
+let kata4='tahun'
+
+// console.log (kata1,angka1,kata2,angka2,kata3,angka2,kata4)
+// let nilaimurid=91
+
+// if (nilaimurid >= 0 && nilaimurid <=30){
+//     console.log('dongo')
+// }
+// else if (nilaimurid >=31 && nilaimurid <=50){
+//     console.log('sedeng')
+// } 
+// else if (nilaimurid >= 51 && nilaimurid <=70){
+//     console.log('pinter')
+// } 
+// else if (nilaimurid >= 71 && nilaimurid <=90){
+//     console.log('pinter bangettttttt')
+// }
+// else if (nilaimurid >=91 && nilaimurid <=100){
+//     console.log('idiooooooooot')
+// }
+// let umuraing=5
+
+// if (umuraing >=30 && umuraing <63){
+//     console.log('panggil aing abang')
+// }
+// else if(umuraing >=63 && umuraing <78){
+//     console.log('panggil aing om')
+// }
+// else if(umuraing >=78 && umuraing <91){
+//     console.log('panggil aing bapaxx')
+// }
+// else if (umuraing >=91 && umuraing <100){
+//     console.log('panggil aing kakek')
+// }
+// else if (umuraing >=100){
+//     console.log('panggil aing buyuttt')
+// }
+// else{
+//     console.log('panggil aing bocah')
+// }
+
+// let umuraing=92
+
+// if (umuraing>=100){
+//     console.log('panggil aing buyuuuuuut')
+// }
+// else if (umuraing>=91){
+//     console.log('panggil aing kakeeeeeee')
+// }
+
+let uangaing= 101000
+let agamaaing= 'islam'
+let usia= 18
+
+if (uangaing <=20000){
+    console.log('kaga cukup bro, buat apaa?')
+}
+else if (uangaing === 20000){
+    console.log('bisaaaa, tapi cuma beli rokok')
+}
+else if (uangaing===43000 && agamaaing === 'islam'){
+    console.log ('dapet roti ama rokok deeeeeh')
+}
+else if (uangaing >=57000 && agamaaing === 'murtad'){
+    console.log('segala dapet roti ama minum')
+}
+else if (uangaing === 72000 || agamaaing === 'muslim'){
+    console.log('dapet nasi padang ama kratindeng')
+}
+else if (uangaing >=100000 && usia <=20 && agamaaing === 'islam'){
+    console.log('bisa beli apa ajaaaaaaa')
+}
+
+    
