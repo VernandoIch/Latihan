a28d335932510666d8b7ded174c506fd1f624871
vernandoich <vernando.ichsan@gmail.com>
2022-08-17 10:25:56 +0700

    latih2

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let kata1= 'nama gue bro nando, umur gue'
-let angka1=5
-let kata2='tahun lagi jadi'
-let angka2=3
-let kata3='puluh'
-let kata4='tahun'
+// let kata1= 'nama gue bro nando, umur gue'
+// let angka1=5
+// let kata2='tahun lagi jadi'
+// let angka2=3
+// let kata3='puluh'
+// let kata4='tahun'
 
 // console.log (kata1,angka1,kata2,angka2,kata3,angka2,kata4)
 // let nilaimurid=91
@@ -53,27 +53,65 @@ let kata4='tahun'
 //     console.log('panggil aing kakeeeeeee')
 // }
 
-let uangaing= 101000
-let agamaaing= 'islam'
-let usia= 18
+// let uangaing= 101000
+// let agamaaing= 'islam'
+// let usia= 18
 
-if (uangaing <=20000){
-    console.log('kaga cukup bro, buat apaa?')
-}
-else if (uangaing === 20000){
-    console.log('bisaaaa, tapi cuma beli rokok')
-}
-else if (uangaing===43000 && agamaaing === 'islam'){
-    console.log ('dapet roti ama rokok deeeeeh')
-}
-else if (uangaing >=57000 && agamaaing === 'murtad'){
-    console.log('segala dapet roti ama minum')
-}
-else if (uangaing === 72000 || agamaaing === 'muslim'){
-    console.log('dapet nasi padang ama kratindeng')
-}
-else if (uangaing >=100000 && usia <=20 && agamaaing === 'islam'){
-    console.log('bisa beli apa ajaaaaaaa')
-}
+// if (uangaing <=20000){
+//     console.log('kaga cukup bro, buat apaa?')
+// }
+// else if (uangaing === 20000){
+//     console.log('bisaaaa, tapi cuma beli rokok')
+// }
+// else if (uangaing===43000 && agamaaing === 'islam'){
+//     console.log ('dapet roti ama rokok deeeeeh')
+// }
+// else if (uangaing >=57000 && agamaaing === 'murtad'){
+//     console.log('segala dapet roti ama minum')
+// }
+// else if (uangaing === 72000 || agamaaing === 'muslim'){
+//     console.log('dapet nasi padang ama kratindeng')
+// }
+// else if (uangaing >=100000 && usia <=20 && agamaaing === 'islam'){
+//     console.log('bisa beli apa ajaaaaaaa')
+// }
 
-    
+let nilainyebudi=92
+let agamanyebudi='islam'
+let kata1='belum kemek'
+let ket2='belum kemem roti'
+let usia=10
+let duitebudi=10000
+let komuk='ganteng'
+
+if (nilainyebudi >20 && nilainyebudi <=35){
+    console.log('budi masih males dan bloon ajeee')
+}
+else if (nilainyebudi >35 && nilainyebudi <=53){
+    console.log('budi ada ada aeh, kocak lau')
+}
+else if (nilainyebudi >53 && nilainyebudi <=60){
+    console.log('ayo dong budddd lebih semangat masa nilainye gitu mulu')
+}
+else if (nilainyebudi >60 && nilainyebudi <=70 && agamanyebudi === 'kristen'){
+    console.log('aduh budi lau udah kristen, tolol pula')
+}
+else if (nilainyebudi >70 && agamanyebudi === 'kristen'){
+    console.log('bud lau iye pintar, tapi cepet mualaf ye')
+}
+else if (nilainyebudi >70 && agamanyebudi === 'kristen'){
+    console.log('sans bud, semangat yaw')
+}
+else if (nilainyebudi >70 && agamanyebudi === 'kristen' && kata1 === 'belum kemek'){
+    console.log('coba bud, kemek dulu gih')
+}
+else if (nilainyebudi >80 && agamanyebudi === 'islam'){
+    console.log('keren bud, kami bangga')
+}
+ else if (nilainyebudi >90 && ket2 === 'belum kemek roti'){
+    console.log('budi kereeeen')
+}
+else if (nilainyebudi === 100 && usia === 10 && duitebudi === 10000 && komuk === 'ganteng'){
+    console.log('budi perfeeeeekkk')
+
+}
