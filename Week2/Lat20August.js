1219c94f738b38a1b8342b400379ad5672bd5949
vernandoich <vernando.ichsan@gmail.com>
2022-08-20 16:37:59 +0700

    belajarngitung

---
+++ Week2/Lat20August.js
@@ -0,0 +1,73 @@
+// function dufan (kodebuk,tinggi,perangkat,waktumain,umur,waktuoperasional){
+//     if (waktuoperasional >9.00){
+//         console.log('boleh antri');
+//         if (kodebuk !== 'false'){
+//             console.log('sudah punya tiket');
+//              console.log('user boleh masuk')
+//              if ( waktuoperasional <21.00){
+//                 if (tinggi >160 && tinggi <200){
+//                     console.log('bisa main hailintar yaaa');
+//                     if (perangkat === 'bawa hp'){
+//                         console.log('dicek dulu yaaa');
+//                     } 
+//                     else {
+//                         console.log('bisa langsung main');
+//                         if (waktumain !== 'tidak selesai'){
+//                             console.log('user harus langsung turun')
+//                         }
+//                         else {
+//                             console.log('dikenakan sanksi');
+//                         }
+//                     }
+                    
+//                 }      
+
+//              }
+//              else {
+//                 console.log('kamu harus pulang');
+//              }
+           
+    
+//         }
+//         else {
+//             console.log('hanya boleh 2-3 wahana');
+//             if (tinggi <160 && umur <15){
+//                 console.log('dapet hadiah, boleh main di dufan malam');
+//             }
+//         }
+        
+//     }
+//     else {
+//         console.log('beli tiket dulu bang');
+//     }
+    
+// }
+
+// dufan(9.10,'false','sudah punya tiket',180)
+
+// function itunganculun (angka1,angka2){
+//     for (let i=angka1; i<= angka2; i+=1){
+//         let hasilmodulo=i%2
+//         if (hasilmodulo === 0){
+//             console.log(i,'angkanya genap') 
+//         }
+//         else{
+//         console.log(i,'angkanya ganjil');
+//         }
+//     }
+// }
+
+// itunganculun(1,10)
+
+// function itungankalender(){ 
+// for (let i=1; i<=31; i++){
+//     if(i%2 === 0){
+//         console.log('plat', i, 'angka genap');
+//     }
+//     else{
+//         console.log('plat', i, 'angka ganjil')
+//         };
+//     }
+// }
+
+// itungankalender()
