391c19cf5d03d8dad761bd25d4e113d22dbaca9c
vernandoich <vernando.ichsan@gmail.com>
2022-08-27 20:08:15 +0700

    belajardeklarasi

---
+++ Week2/Lat24Agustus.js
@@ -0,0 +1,104 @@
+// // Deklarasi
+// // let nama = "Nando"; // String
+// let umur = 30; // Number
+// let laki = true; // Boolean
+
+// // Deklarasi Objek
+// let data = {
+//   nama: "Rehan", // jadi Ole
+//   umur: 30,
+//   laki: true,
+//   hoby: "Maen cewe",
+// };
+
+// // index dimulai dari 0
+// // length dimulai dari 1
+
+// // Deklarasi Array 3
+// let biodata = ["Ole", 30, "Es krim"];
+
+// // biodata.map((el, index) => {
+// //   console.log(el, index);
+// // });
+
+// // for (let i = 0; i < biodata.length; i++) {
+// //   console.log(biodata[i], i);
+// // }
+
+// // console.log(biodata.length);
+
+// // function pembagian(angka1, angka2) {
+// //   let jumlah = angka1 / angka2;
+// //   let hasil = jumlah % 2;
+// //   if (hasil === 0) {
+// //     console.log("Genap", jumlah);
+// //   } else {
+// //     console.log("Ganjil", jumlah);
+// //   }
+// // }
+
+// // pembagian(14, 7);
+
+// // function perkalian(angka1, angka2) {
+// //   let hasil = angka1 * angka2;
+// //   let jumlah = hasil % 2;
+// //   if (jumlah === 1) {
+// //     console.log("Ganjil", hasil);
+// //   } else {
+// //     console.log("Genap", hasil);
+// //   }
+// // }
+
+// // perkalian(5, 3);
+
+// // function tambahhh(nol1, nol2) {
+// //   let hasil = nol1 + nol2;
+// //   let jumlah = hasil % 2;
+// //   let anakangkatanapa;
+// //   if (jumlah === 0) {
+// //     anakangkatanapa = "Genap";
+// //     console.log("Anak", anakangkatanapa);
+// //   } else {
+// //     anakangkatanapa = "Ganjil";
+// //     console.log("Anak", anakangkatanapa);
+// //   }
+// // }
+
+// // tambahhh(6, 1);
+
+// // for (let i = 0; i < 100; i += 8) {
+// //   let hasil = i % 2;
+// //   if (hasil === 0) {
+// //     console.log(i, "adalah angka genap");
+// //   } else {
+// //     console.log(i, "adalah angka ganjil");
+// //   }
+// // }
+
+// // for (let i = 12; i <= 32; i += 1) {
+// //   if (i === 0) {
+// //     console.log(i);
+// //   } else {
+// //     if (i % 6 === 0) {
+// //       console.log(i);
+// //     }
+// //   }
+// // }
+
+// // for (let i = 0; i < 50; i++) {
+// //   if (i <= 5) {
+// //     console.log(i, "masih balita");
+// //   } else if (i > 5 && i <= 10) {
+// //     console.log(i, "bawah sepuluh tahun");
+// //   } else {
+// //     console.log("udah gede");
+// //   }
+// // }
+
+// let nama = "Kadit";
+
+// for (let i = nama.length - 1; i >= 0; i--) {
+//   nama += nama[i];
+// }
+
+// console.log(nama);
