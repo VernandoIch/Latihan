025cf488cd055029815358856db9fa5be305670f
vernandoich <vernando.ichsan@gmail.com>
2022-09-14 20:16:05 +0700

    belajar looping 2

---
+++ Week 4/14Sep.js
@@ -0,0 +1,36 @@
+// function waktu(time){
+//     let menit=time/60
+//     let jam=time / menit
+//     let detik=time%60
+//     let pembulatan=Math.floor(menit)
+
+//     console.log(jam,':',pembulatan,':',detik);
+// }
+
+// waktu(433)
+
+// function judi (){
+//     let angkarandom=Math.floor(Math.random()*6+1)
+//     console.log(angkarandom,'hasilnya');
+//     if(angkarandom===1){
+//         console.log('coba lagi ya');
+//     }
+//     else if (angkarandom===2){
+//         console.log('selamat kamu mendapatkan kupon sebanyak 5');
+//     }
+//     else if (angkarandom===3){
+//         console.log('selamat kamu mendapatkan kupon sebanyak 15')
+//     }
+//     else if (angkarandom===4){
+//         console.log('selamat kamu mendapatkan kupon sebanyak 50')
+//     }
+//     else if (angkarandom===5){
+//         console.log('selamat kamu mendapatkan kupon sebanyak 100')
+//     }
+// }
+
+// judi()
+
+for(let i=50; i>=1; i--){
+    console.log(i,'I love coding');
+}
