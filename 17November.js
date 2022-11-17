3de52874200ac1d9344ddcfd383e0efd3f7712d6
vernandoich <vernando.ichsan@gmail.com>
2022-11-17 17:26:34 +0700

    recap

---
+++ 17November.js
@@ -0,0 +1,40 @@
+// let nama = 'nando'
+
+// console.log(nama);
+
+// for (let angka = 1; angka <10; angka +=1){
+//     console.log(angka);
+// }
+
+// for (let angka = 1; angka <11; angka +=1){
+//     if (angka%2===0){
+//         console.log(angka,"adalah angka genap");
+//     }
+//     else {
+//         console.log(angka,"adalah angka ganjil");
+//     }
+// }
+
+// for (let angka=1; angka<6; angka +=1){
+//     console.log('nando');
+// }
+
+// xoxoxoxxx
+
+// for (let i=)
+
+//bima krishna
+
+let nama = "xoxoxoxxx"
+let jumlahx = 0
+let jumlaho = 0
+for(let i = 1; i<nama.length; i++){
+    if(nama[i] === "x"){
+        jumlahx +=1
+    } else {
+        jumlaho += 1
+    }
+}
+
+console.log("jumlah x adalah", jumlahx);
+console.log("jumlah o adalah", jumlaho);
